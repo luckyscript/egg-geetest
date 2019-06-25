6424df9ee4a887711a466380552839c7c60602c9
luckyscript <main.lukai@gmail.com>
2019-06-25 14:01:11 +0800

    init

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+import * as Geetest from 'gt3-sdk';
+
+interface EggGeetestOptions {
+  geetest_id: string;
+  geetest_key: string;
+  Geetest?: Geetest;
+}
+
+declare module 'egg' {
+  interface Application {
+    geetest: Geetest & Singleton<Geetest>;
+  }
+
+  interface EggAppConfig {
+    geetest: EggGeetestOptions;
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const assert = require('assert');
-const awaitFirst = require('await-first');
 
 module.exports = app => {
   app.addSingleton('geetest', createGeetest);
@@ -9,39 +8,39 @@ module.exports = app => {
 
 function createGeetest(config, app) {
   const Geetest = app.config.geetest.Geetest || require('gt3-sdk');
-  assert(config.geetest_id && config.geetest_key, '[egg-geetest] geetest_id and geetest_key is required')
+  assert(config.geetest_id && config.geetest_key, '[egg-geetest] geetest_id and geetest_key is required');
   const { geetest_id, geetest_key } = config;
   const instance = new Geetest({ geetest_id, geetest_key });
   instance.registerPromise = () => {
-    return new Promise((res, rej) => instance.register(null, (err, data)=> {
+    return new Promise((res, rej) => instance.register(null, (err, data) => {
       if (err) {
         rej(err);
       }
       res(data);
-    }))
+    }));
   };
-  
+
   instance.validatePromise = (gt, gtSec) => {
-    return new Promise((resolve, reject) => 
-      click.validate(gt, gtSec, (err, success) => {
+    return new Promise((resolve, reject) =>
+      instance.validate(gt, gtSec, (err, success) => {
         if (err) {
           reject(err);
         }
         if (!success) {
-          reject('验证码已过期');
+          reject(new Error('验证码已过期'));
         }
         resolve('success');
       })
     );
   };
 
-  instance.twoStepCheck = ({ 
-    gt, 
-    geetest_challenge, 
-    geetest_validate, 
-    geetest_seccode 
+  instance.twoStepCheck = ({
+    gt,
+    geetest_challenge,
+    geetest_validate,
+    geetest_seccode,
   }) => instance.validatePromise(gt, { geetest_challenge, geetest_validate, geetest_seccode });
-  
+
   return instance;
 }
 
